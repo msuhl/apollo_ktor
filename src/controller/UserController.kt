7f438b73621b98c48e2e207d23ef64d83e0cc991
mjun <gate.nielsen@gmail.com>
2020-10-22 21:58:59 +0200

    Use UserOneQuery from autogenerated class

--- src/controller/UserController.kt
+++ src/controller/UserController.kt
@@ -1,21 +1,22 @@
 package com.example.controller
 
+import UserOneQuery
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.toDeferred
 import com.apollographql.apollo.exception.ApolloException
 
 class UserController {
-    lateinit var apolloClient: ApolloClient
+     var apolloClient: ApolloClient
     init{
         apolloClient = ApolloClient.builder()
             .serverUrl("https://fakeql.com/graphql/63c186327645e3a55dee03419de4d557")
             .build()
     }
 
-    fun getUserOne():String{
+    suspend fun getUserOne():String{
 
-//        val query = TestQuery()
         val response = try {
-//            apolloClient.query(userOne).toDeferred().await()
+            apolloClient.query(UserOneQuery()).toDeferred().await()
         } catch (e: ApolloException) {
             print("Query catched")
         }
