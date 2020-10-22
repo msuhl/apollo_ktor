a04f0f7269102a690d63d983f0303bfdab37c71d
mjun <gate.nielsen@gmail.com>
2020-10-22 21:05:00 +0200

    Add imit and apolloClient to UserController.kt

--- src/controller/UserController.kt
+++ src/controller/UserController.kt
@@ -1,8 +1,13 @@
 package com.example.controller
 
-class UserController {
-    init{
+import com.apollographql.apollo.ApolloClient
 
+class UserController {
+    lateinit var apolloClient: ApolloClient
+    init{
+        apolloClient = ApolloClient.builder()
+            .serverUrl("https://fakeql.com/graphql/63c186327645e3a55dee03419de4d557")
+            .build()
     }
 
     fun getUserOne():String{
